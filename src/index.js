eeea4ad634bf0c2415b488ce3df33c6cdf250501
Daniel <oscar.d.gonzalez@gmail.com>
2024-04-11 22:01:59 -0500

    Add babel modules and react dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Empezamos yah!');
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<h1>Hello World!</h1>);
