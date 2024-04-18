8bc438189054bcb0d4e16134cb61fe01e4568eda
Daniel <oscar.d.gonzalez@gmail.com>
2024-04-17 21:17:48 -0500

    feat: :sparkles: 'ListOfCategories' component basic structure and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyles />
+    <ListOfCategories />
   </>
 )
