ac95c2df87e729af557e17aa256d7c589465ecc7
Daniel <oscar.d.gonzalez@gmail.com>
2024-04-30 18:08:09 -0500

    feat: :sparkles: 'ListOfCategories' data fetching from local JSON file

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Category } from '../Category'
 import { Item, List } from './style'
+import data from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
-      {[1, 2, 3, 4].map(category => (
-        <Item key={category}><Category /></Item>
+      {data.categories.map(category => (
+        <Item key={category.id}><Category {...category} /></Item>
       ))}
     </List>
 
