f8d09f9aaacb3fa425561bf9130c4a313a6b3fe0
Daniel <oscar.d.gonzalez@gmail.com>
2024-04-30 22:12:06 -0500

    feat: :sparkles: fetching categories from api

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './style'
-import data from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('https://petgram-server-odagora.vercel.app/categories')
+      .then(data => data.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
-      {data.categories.map(category => (
+      {categories.map(category => (
         <Item key={category.id}><Category {...category} /></Item>
       ))}
     </List>
