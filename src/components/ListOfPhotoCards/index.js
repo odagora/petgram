a5eb9342dc2c4af57a2c9d7a6ad85bd108fa23d9
Daniel <oscar.d.gonzalez@gmail.com>
2024-07-15 20:09:55 -0500

    feat: :sparkles: PhotoCard lazy loading using intersectionObserver

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -4,7 +4,7 @@ import { PhotoCard } from '../PhotoCard'
 export const ListOfPhotoCards = () => {
   return (
     <ul>
-      {[1, 2, 3].map(card => (
+      {[1, 2, 3, 4, 5].map(card => (
         <PhotoCard key={card} />
       ))}
     </ul>
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import { Button, Img, ImgWrapper } from './style'
+import React, { useEffect, useRef, useState } from 'react'
+import { Button, Img, ImgWrapper, Article } from './style'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518001589401-1743b61d1def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const element = ref.current
+    const observer = new window.IntersectionObserver(entries => {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element)
+    return () => observer.disconnect()
+  }, [ref])
+
   return (
-    <article>
-      <a href={`details/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt='image' />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' />{likes} likes!
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {show && (
+        <>
+          <a href={`details/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt='image' />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' />{likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   )
 }
