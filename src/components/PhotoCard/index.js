36db75df6cb16d9ce889484349a2d79cb7f17df2
Daniel <oscar.d.gonzalez@gmail.com>
2024-07-26 22:52:43 -0500

    refactor: :recycle: 'useLocalStorage' custom hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { Button, Img, ImgWrapper, Article } from './style'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useOnScreen } from '../../hooks/useOnScreen'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518001589401-1743b61d1def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,14 +11,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const isOnScreen = useOnScreen(ref)
   const [show, setShow] = useState(false)
   const key = `photo-card-${id}`
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key)
-      return like !== null ? JSON.parse(like) : false
-    } catch (error) {
-      return false
-    }
-  })
+  const [liked, setLiked] = useLocalStorage(key, false)
 
   useEffect(() => {
     if (isOnScreen) {
@@ -26,15 +20,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   }, [isOnScreen])
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
-
-  const setLocalStorage = (value) => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(value))
-      setLiked(value)
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <Article ref={ref}>
@@ -45,7 +30,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} alt='image' />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLocalStorage(!liked)}>
+          <Button onClick={() => setLiked(!liked)}>
             <Icon size='32px' />{likes} likes!
           </Button>
         </>
